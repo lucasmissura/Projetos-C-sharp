967c55eef9aebe3f937cc25894e6d1c2195823f3
lucasmissura <lucas.missura29@gmail.com>
2024-01-23 19:56:51 -0300

    Create Program (identificador de número da casa).cs
    Neste projeto foi utilizado um código que identifica a numeração da casa digitada

---
+++ Program (identificador de número da casa).cs
@@ -0,0 +1,6 @@
+﻿Console.WriteLine("digite o número da sua casa ");
+string numeroCasa  = Console.ReadLine();
+int numero = int.Parse(numeroCasa);
+Console.WriteLine("O número da sua casa é {0}" , numero);
+
+numero = Convert.ToInt32(numeroCasa);
