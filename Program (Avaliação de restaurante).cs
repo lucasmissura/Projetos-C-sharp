b81ffd21e2938230aa2808ebca0879f5a53884ad
lucasmissura <lucas.missura29@gmail.com>
2024-01-23 20:09:52 -0300

    projeto avaliação de restaurante
    Neste projeto foi realizado a criação de um código onde é possível dar uma avaliação para o prato X de um rastaurante

---
+++ Program (Avaliação de restaurante).cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Qual a nota que você daria para o prato? 0, 5 ou 10");
+int.TryParse(Console.ReadLine(), out int nota);
+
+switch (nota)
+{
+    case 0:
+        Console.WriteLine("Comida muito ruim!");
+        break;
+    case 5:
+        Console.WriteLine("Comida razoavel!");
+        break;
+    case 10:
+        Console.WriteLine("Comida muito boa!");
+        break;
+    default:
+        Console.WriteLine("Nota inexistente!");
+        break;
+}
