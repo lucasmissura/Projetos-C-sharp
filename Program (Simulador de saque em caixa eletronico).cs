60f7d772dacd512355acc0bb690bb6502f676f41
lucasmissura <lucas.missura29@gmail.com>
2024-01-23 20:05:05 -0300

    Create Program (Simulador de saque em caixa eletronico).cs
    Neste projeto foi criado um simulador onde é possível realizar um saque na conta corrente conforme o código.

---
+++ Program (Simulador de saque em caixa eletronico).cs
@@ -0,0 +1,26 @@
+﻿string simNao;
+double saque, saldo;
+
+Console.WriteLine("Bem vindo, por favor insira o cartão");
+Console.WriteLine("Digite o número da sua conta corrente");
+Console.ReadLine();
+Console.WriteLine("Digite o número da sua agência");
+Console.ReadLine();
+Console.WriteLine("Você deseja realizar um saque?");
+simNao = Console.ReadLine();
+saldo = 800;
+
+if (simNao == "S")
+{
+    Console.WriteLine("Qual valor você deseja sacar?");
+    saque = double.Parse(Console.ReadLine());
+    saldo = saldo - saque;
+    Console.WriteLine(saldo);
+    {
+        if (saque > saldo)
+        {
+            Console.WriteLine("Saldo insuficiente, retire seu cartão");
+        }
+
+    }
+}
